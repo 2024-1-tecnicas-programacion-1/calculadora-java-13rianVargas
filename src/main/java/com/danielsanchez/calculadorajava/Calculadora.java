caefe14098f569b06fc70f6347ca39c928e134b7
JosephElMejor <josephy.mendietaa@konradlorenz.edu.co>
2024-02-26 09:29:05 -0500

    Se agrega operaciones de potencia y residuo.

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -26,9 +26,14 @@ public class Calculadora {
                 // Operación división:
                 respuesta = (double) numero1 / numero2;
                 break;
-            
-            // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
-                
+            case '^' :
+                // Operación potencia:
+                respuesta = (double) Math.pow(numero1,numero2);
+                break;
+            case '%' :
+                // Operación residuo:
+                respuesta = (double) numero1 % numero2;
+                break;   
             default:
                 throw new IllegalArgumentException("Operación inválida.");
         }
